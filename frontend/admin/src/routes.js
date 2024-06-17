22979f6700c241b7d7a1bdee6684746a64ab542f
KevinPoehlmann <poehlmann.kevin@gmail.com>
2024-06-17 14:39:21 +0200

    actually solving the import issue

--- frontend/admin/src/routes.js
+++ frontend/admin/src/routes.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
-import Patchloader from "./views/patchloader/Patchloader.vue";
+import Patchloader from "./views/patchloader/PatchLoader.vue";
 import Dataoverview from "./views/dataoverview/Dataoverview.vue";
 import Champion from "./views/editor/Champion.vue";
 import Item from "./views/editor/Item.vue";
